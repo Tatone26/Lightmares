9dea6714ab7fd08a2f473b39f8c93b502efdffcb
Tatone26 <anatolesainero@gmail.com>
2021-09-24 23:17:30 +0200

    bouh
    ye
    
    n

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -4,88 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public Animator animator;
+    public Animator animator;
 
-  public float moveSpeed = 5f;
-  public Transform movePoint;
+    public float moveSpeed = 5f;
+    public Transform movePoint;
 
-  public LayerMask whatStopMovement;
+    public LayerMask whatStopMovement;
 
-  public float moveRate = 1f;
-  public float moveTime = 0f;
-  public float nextActionTime = 0f;
+    public float moveRate = 1f;
+    public float moveTime = 0f;
+    public float nextActionTime = 0f;
 
-
-  // Start is called before the first frame Update
-  void Start()
-  {
-    movePoint.parent = null; 
-  }
-
-  void Movex() //move code
-  {
-    if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+    // Start is called before the first frame Update
+    void Start()
     {
-      if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-      {
-        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
-        animator.SetFloat("Vertical", 0f);
-        moveTime += Time.deltaTime;
-
-        if (moveTime >= 0.125f)
-        {
-          if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopMovement))
-          {
-            animator.SetFloat("Speed", 1f);
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-            nextActionTime = Time.time + 0.5f / moveRate;
-          }
-        }
-      } 
+        movePoint.parent = null;
     }
-  }
-  void Movey() //move code
-  {
-    if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+
+    Vector3 getMoveVector(string direction)
     {
-      if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-      {
-        animator.SetFloat("Horizontal", 0f);
-        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
-        moveTime += Time.deltaTime;
-        
-        if (moveTime >= 0.125f)
+        if (direction == "Vertical")
         {
-          if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopMovement))
-          {
-            animator.SetFloat("Speed", 1f);
-            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-            nextActionTime = Time.time + 0.5f / moveRate;
-          }
+            return new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
         }
-      }
-    }   
-  }
+        else
+        {
+            return new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        }
+    }
+
+    void Move(string direction) //move code
+    {
+        string notdir = "Horizontal";
+        if (direction == "Horizontal")
+        {
+            notdir = "Vertical";
+        }
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw(direction)) == 1f)
+            {
+                animator.SetFloat(direction, Input.GetAxisRaw(direction));
+                animator.SetFloat(notdir, 0f);
+                moveTime += Time.deltaTime;
+
+                if (moveTime >= 0.125f)
+                {
+                    if (!Physics2D.OverlapCircle(movePoint.position + getMoveVector(direction), .2f, whatStopMovement))
+                    {
+                        animator.SetFloat("Speed", 1f);
+                        movePoint.position += getMoveVector(direction);
+                        nextActionTime = Time.time + 0.5f / moveRate;
+                    }
+                }
+            }
+        }
+    }
 
     //Update is called once per frame
     void Update()
-  {
-    transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-    if ((Time.time >= nextActionTime) && (animator.GetBool("Attaque") == false))
-    {
-      Movey();
-      Movex();
+        if ((Time.time >= nextActionTime) && (animator.GetBool("Attaque") == false))
+        {
+            Move("Horizontal");
+            Move("Vertical");
+        }
+        if ((Input.GetAxisRaw("Horizontal") == 0f) && (Input.GetAxisRaw("Vertical") == 0f))
+        {
+            moveTime = 0;
+        }
+        if (Vector3.Distance(transform.position, movePoint.position) == 0f)
+        {
+            animator.SetFloat("Speed", 0f);
+        }
     }
-    if ((Input.GetAxisRaw("Horizontal")== 0f) && (Input.GetAxisRaw("Vertical") == 0f))
-    {
-      moveTime = 0;
-    }
-    if (Vector3.Distance(transform.position, movePoint.position) == 0f)
-    {
-      animator.SetFloat("Speed", 0f);
-    }
-
-    
-  }
 }
