2ae2f255f4311c11e4bf9ae1b6f801a1e566e129
Tatone26 <anatolesainero@gmail.com>
2021-12-02 18:45:15 +0100

    Plein de petits trucs
    mouvement sans grille, propreté, boxcollider pour futures interactions...

---
+++ Assets/Code/Inventory/InventoryApparition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryApparition : MonoBehaviour
+{
+    public GameObject worldCamera;
+    private Image inventoryImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            inventoryImage.enabled = !inventoryImage.enabled;
+        }
+    }
+}
--- Assets/Code/PlayerMeleAttack.cs
+++ Assets/Code/PlayerMeleAttack.cs
@@ -4,102 +4,96 @@ using UnityEngine;
 
 public class PlayerMeleAttack : MonoBehaviour
 {
-  public Animator animator ;
-  public Transform attackPoint;
-  public float attackRange = 0.4f;
-  public int attackDamage = 40;
-  public LayerMask ennemyLayers;
-  public Transform movePoint;
+    public Animator animator;
 
-  public float attackRate = 1f;
-  public float attackTime = 0f;
-  public float nextActionTime = 0f;
+    public Transform attackPoint;
+    public float attackRange = 0.4f;
+    public int attackDamage = 40;
+    public LayerMask ennemyLayers;
 
+    public float attackRate = 1f;
+    public float attackTime = 0f;
+    public float nextActionTime = 0f;
+    public Rigidbody2D rb;
 
-  //Update is called once per frame
-  void Update()
-  {
-    bool attaque = Input.GetKeyDown(KeyCode.Mouse0);
-    attackPoint.position = movePoint.position + new Vector3(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"), 0f);
-
-    if (Time.time >= nextActionTime){
-
-      if (animator.GetFloat("Speed") == 0){
-        animator.SetBool("Attaque", attaque); // Animation attaque
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-          DaggerAttack();
-          nextActionTime = Time.time + 0.25f / attackRate;
-        }
-      }
-    }
-    
-  }
-
-  void DaggerAttack () {
-    // Hitbox de l'attaque
-    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, ennemyLayers);
-    // infligeage des damage
-    foreach (Collider2D enemy in hitEnemies) 
+    //Update is called once per frame
+    void Update()
     {
-      Debug.Log ("Touché");
-      enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
-    }
-          
-  }
+        bool attaque = Input.GetKeyDown(KeyCode.Mouse0);
+        attackPoint.position = rb.position + new Vector2(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"));
 
-  //void SwordAttack () {
+        if (Time.time >= nextActionTime)
+        {
+            if (animator.GetFloat("Speed") == 0)
+            {
+                animator.SetBool("Attaque", attaque); // Animation attaque
+
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    DaggerAttack();
+                    nextActionTime = Time.time + 0.25f / attackRate;
+                }
+            }
+        }
+    }
+
+    void DaggerAttack()
+    {
+        // Hitbox de l'attaque
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, ennemyLayers);
+        // infligeage des damage
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Touché");
+            enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
+        }
+    }
+
+    //void SwordAttack () {
     //Vector3 direction = transform.TransformDirection(animator.GetFloat("Vertical"), animator.GetFloat("Horizontal"), 0f);
     //RaycastHit2D[] hitEnemies = Physics2D.RaycastAll(attackPoint.position - direction, direction*2, ennemyLayers);
-  
-    //foreach (RaycastHit2D enemy in hitEnemies) 
+
+    //foreach (RaycastHit2D enemy in hitEnemies)
     //{
-     //Debug.Log ("Touché");
-     //enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
+    //Debug.Log ("Touché");
+    //enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
     //}
-  //}
-  
-  //void SpearAttack() {
+    //}
+
+    //void SpearAttack() {
     //Vector3 direction = transform.TransformDirection(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"), 0f);
     //RaycastHit2D[] hitEnemies = Physics2D.RaycastAll(attackPoint.position, direction);
-  
-    //foreach (RaycastHit2D enemy in hitEnemies) 
+
+    //foreach (RaycastHit2D enemy in hitEnemies)
     //{
-     //Debug.Log ("Touché");
-     //enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
+    //Debug.Log ("Touché");
+    //enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
     //}
-  //}
-  
-  void AxeAttack() {
-    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(movePoint.position, attackRange*3, ennemyLayers);
-    foreach (Collider2D enemy in hitEnemies) 
+    //}
+
+    void AxeAttack()
     {
-     Debug.Log ("Touché");
-     enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(rb.position, attackRange * 3, ennemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Touché");
+            enemy.GetComponent<EnememiesLife>().TakeDamage(attackDamage);
+        }
     }
-  }
-  
-  void OnDrawGizmosSelected ()
-  {
-    if (attackPoint == null)
-      return;
-    
-    //Vector3 direction = transform.TransformDirection(animator.GetFloat("Vertical"), animator.GetFloat("Horizontal"), 0f); // épée
-    //Gizmos.DrawRay(attackPoint.position - direction, direction*2);
 
-    //Vector3 direction = transform.TransformDirection(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"), 0f); // Lance
-    //Gizmos.DrawRay(attackPoint.position, direction);
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
 
-    Gizmos.DrawWireSphere (movePoint.position, attackRange*3); //hache
-    
-    //Gizmos.DrawWireSphere(attackPoint.position, attackRange); // Dague
-  }
+        //Vector3 direction = transform.TransformDirection(animator.GetFloat("Vertical"), animator.GetFloat("Horizontal"), 0f); // épée
+        //Gizmos.DrawRay(attackPoint.position - direction, direction*2);
 
-  
+        //Vector3 direction = transform.TransformDirection(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"), 0f); // Lance
+        //Gizmos.DrawRay(attackPoint.position, direction);
 
- 
+        Gizmos.DrawWireSphere(rb.position, attackRange * 3); //hache
 
-
+        //Gizmos.DrawWireSphere(attackPoint.position, attackRange); // Dague
+    }
 }
-
